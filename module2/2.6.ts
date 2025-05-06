4ca34ed1bc17ade65e0af9f8b067a47391eec2ea
devsafix <devsafix@gmail.com>
2025-05-06 21:11:13 +0600

    Modify 2.6

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -1,58 +1,60 @@
 {
-  //
-  // constraints in TypeScript
-  const addCourseToStudent = <T extends { id: number; name: string }>(
-    student: T
-  ) => {
-    const course = "NLWD";
-    return { ...student, course };
-  };
-  const student1 = addCourseToStudent({
-    id: 111,
-    name: "hello",
-    age: 1,
-    devType: "js",
-  });
-  const student2 = addCourseToStudent({
-    id: 222,
-    name: "hello",
-    age: 1,
-    hasWatch: "yes",
-  });
-  const student3 = addCourseToStudent({
-    id: 333,
-    name: "hello",
-    pookie: "hello",
-  });
+  {
+    //
+    // constraints in TypeScript
+    const addCourseToStudent = <T extends { id: number; name: string }>(
+      student: T
+    ) => {
+      const course = "NLWD";
+      return { ...student, course };
+    };
+    const student1 = addCourseToStudent({
+      id: 111,
+      name: "hello",
+      age: 1,
+      devType: "js",
+    });
+    const student2 = addCourseToStudent({
+      id: 222,
+      name: "hello",
+      age: 1,
+      hasWatch: "yes",
+    });
+    const student3 = addCourseToStudent({
+      id: 333,
+      name: "hello",
+      pookie: "hello",
+    });
 
-  //generic constraints with keyof
-  type Vehicle = {
-    bike: string;
-    car: string;
-    ship: string;
-  };
+    //generic constraints with keyof
+    type Vehicle = {
+      bike: string;
+      car: string;
+      ship: string;
+    };
 
-  type Owner = "bike" | "car" | "ship";
+    type Owner = "bike" | "car" | "ship";
 
-  type Owner2 = keyof Vehicle; // "bike" | "car" | "ship"
+    type Owner2 = keyof Vehicle; // "bike" | "car" | "ship"
 
-  const person1: Owner = "bike"; // ok
-  const person2: Owner2 = "ship"; // ok
+    const person1: Owner = "bike"; // ok
+    const person2: Owner2 = "ship"; // ok
 
-  const user = {
-    name: "hello",
-    age: 1,
-    address: "tng",
-  };
+    const user = {
+      name: "hello",
+      age: 1,
+      address: "tng",
+    };
 
-  //   user['name'] = "hello" // ok
+    //   user['name'] = "hello" // ok
 
-  function getProperty<T, K extends keyof T>(obj: T, key: K) {
-    return obj[key];
+    function getProperty<T, K extends keyof T>(obj: T, key: K) {
+      return obj[key];
+    }
+
+    const result1 = getProperty(user, "name"); // ok
+    const result2 = getProperty(user, "age"); // ok
+
+    //
   }
-
-  const result1 = getProperty(user, "name"); // ok
-  const result2 = getProperty(user, "age"); // ok
-
-  //
 }
